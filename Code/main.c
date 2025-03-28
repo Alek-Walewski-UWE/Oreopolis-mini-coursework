79201be2df711c292e9579accc270c16154accd2
Alek.Walewski <aleksander2.walewski@live.uwe.ac.uk>
2025-03-28 11:51:25 +0000

    28/03/2025 - Completed map generation part of initial setup function

--- Code/main.c
+++ Code/main.c
@@ -4,42 +4,123 @@
 #include "main.h"
 
 int main(){
-    char mainMenuOption;
     char readChar;
+    int mainMenuOption = 0;
+    cell map[mapSize][mapSize] = {};
 
     mainMenu:
-    mainMenuOption = 0;
-    readChar = 0;
     system("cls");
     // Open file with introduction text
     currentFile = fopen(introductionFile, "r");
     // Loop through and print each char until end of file is reached
-    while((readChar = fgetc(currentFile)) != EOF){
+    while((readChar = (char)fgetc(currentFile)) != EOF){
         printf("%c", readChar);
     }
     fclose(currentFile);
 
     // Allow user to choose menu option
-    while(mainMenuOption == NULL){
-        if(kbhit){
-            mainMenuOption = getch();
-            switch((int)mainMenuOption){
-                case 49:
-                    // Prompt user to select difficulty
-                    // Call initial game setup function
-                    break;
-                case 50:
-                    // Call function to read leaderboard
-                    break;
-                case 51:
-                    return 0;
-                default:
-                    mainMenuOption = NULL;
-                    break;
-            }
+    while(mainMenuOption == 0){
+        mainMenuOption = getKey();
+        switch(mainMenuOption){
+            case 49:
+                // Call initial game setup function
+                gameSetup(&map);
+                break;
+            case 50:
+                // Call function to read leaderboard
+
+                goto mainMenu;
+                break;
+            case 51:
+                return 0;
+            default:
+                mainMenuOption = 0;
+                break;
+        }
+    }
+    return 0;
+}
+
+void gameSetup(cell (*mapToInit)[mapSize]){
+    int difficulty = 0, impassables, x, y;
+    float gemRarityFactor, gemProbability;
+    // Prompt user to select difficulty
+    printf("Please select one of the following difficulties: %s%s%s", easyText, normalText, hardText);
+    while(difficulty == 0){
+        difficulty = getKey();
+        switch (difficulty) {
+            case 49:
+                // Initiate easy difficulty variables
+                gemRarityFactor = easyRarityFactor;
+                impassables = easyImpassables;
+                break;
+            case 50:
+                // Initiate normal difficulty variables
+                gemRarityFactor = normalRarityFactor;
+                impassables = normalImpassables;
+                break;
+            case 51:
+                // Initiate hard mode difficulty
+                gemRarityFactor = hardRarityFactor;
+                impassables = hardImpassables;
+                break;
+            default:
+                difficulty = 0;
+                break;
         }
     }
 
+    // Place shop in cell (0,0)
 
-    return 0;
+    for (int x = 0; x < mapSize; ++x) {
+        for (int y = 0; y < mapSize; ++y) {
+            mapToInit[x][y] = uninitialised;
+        }
+    }
+    mapToInit[0][0] = shopCell;
+
+    // Place impassable cells at random map locations
+    while(impassables != 0){
+        srand(time(NULL));
+        x = rand() % mapSize;
+        y = rand() % mapSize;
+        // Check if cell is already populated
+        if(mapToInit[x][y].icon != 'U'){
+            continue;
+        }else{
+            mapToInit[x][y] = impassableCell;
+            impassables--;
+        }
+    }
+
+    // Loop through each cell and place a random cell
+    for (x = 0; x < mapSize; ++x) {
+        for (int y = 0; y < mapSize; ++y) {
+            if(mapToInit[x][y].icon != 'U'){
+                continue;
+            }else{
+                // Use probability of each gem/ore to decide what gets placed in the cell
+                // Diamond
+                gemProbability = (rand() % 100) + 1;
+                if(gemProbability <= (gemRarityFactor * diamond.item.rarity * 100)){
+                    mapToInit[x][y] = diamond;
+                    continue;
+                }
+                // Gold Ore
+                gemProbability = (rand() % 100) + 1;
+                if(gemProbability <= (gemRarityFactor * goldOre.item.rarity * 100)){
+                    mapToInit[x][y] = goldOre;
+                    continue;
+                }
+                // Iron Ore
+                gemProbability = (rand() % 100) + 1;
+                if(gemProbability <= (gemRarityFactor * ironOre.item.rarity * 100)){
+                    mapToInit[x][y] = ironOre;
+                    continue;
+                }
+
+                mapToInit[x][y] = ground;
+            }
+        }
+    }
 }
--- Code/main.h
+++ Code/main.h
@@ -12,6 +12,39 @@
 #include <conio.h>
 #include <time.h>
 
+#define introductionFile "../Code/introduction.txt"
+FILE* currentFile;
+
+#define easyText "\nEasy(1): 2x chance of rare gems, 200 energy, no impassable cells\n"
+#define easyRarityFactor 2
+#define easyImpassables 0
+#define normalText "\nNormal(2): 1x chance of rare gems, 150 energy points, 2 impassable cells\n"
+#define normalRarityFactor 1
+#define normalImpassables 2
+#define hardText "\nHard(3): 0.75x chance of rare gems, 100 energy points, 4 impassable cells\n"
+#define hardRarityFactor 0.75
+#define hardImpassables 4
+
+#define mapSize 10
+
+typedef struct mineableItem{
+    float value, rarity, weight;
+}mineableItem;
+
+typedef struct cell{
+    int mineable;
+    mineableItem item;
+    char name[15], icon;
+} cell;
+
+cell uninitialised = {.icon='U'};
+cell shopCell = {.name="Shop", .icon='$', .mineable=0,};
+cell impassableCell = {.name="Impassable",.icon='X', .mineable=0,};
+cell ironOre = {.name="Iron Ore", .icon='I', .mineable=1, .item={.value=1, .weight=1, .rarity = 0.1}};
+cell goldOre = {.name="Gold Ore", .icon='G', .mineable=1, .item={.value=2, .weight=2, .rarity = 0.05}};
+cell diamond = {.name="Diamond", .icon='D', .mineable=1, .item={.value=5, .weight=5, .rarity = 0.01}};
+cell ground = {.name="Ground", .icon=' ', .mineable=0};
+
 // Delay function to allow for time delay between events
 void delay(int delayTime){
     // Convert input in seconds to milliseconds
@@ -19,9 +52,16 @@ void delay(int delayTime){
     // Store current time
     clock_t startTime = clock();
     // Loop until wanted time has passed
-    while (clock() < startTime + milliSeconds)
-        ;
+    while (clock() < startTime + milliSeconds);
 }
 
-#define introductionFile "../Code/introduction.txt"
-FILE* currentFile;
+// Function to wait for key press and return the ascii value of the key pressed
+int getKey(){
+    while(1){
+        if(kbhit()){ // check if keyboard hit
+            return getch();
+        }
+    }
+}
+
+void gameSetup(cell (*mapToInit)[mapSize]);
